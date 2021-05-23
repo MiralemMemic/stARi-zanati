a3b1748dfe2455dd89f5a441fe819a866a77583f
MiralemMemic <mmemic2@etf.unsa.ba>
2021-05-23 13:07:52 +0200

    Initial commit

---
+++ Assets/PlayVideoOnTap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideoOnTap : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    Ray ray;
+    RaycastHit hit;
+    
+    void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer> ();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.Play();
+        videoPlayer.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(videoPlayer.isPlaying)
+                {
+                    videoPlayer.Pause();
+                }
+                else
+                {
+                    videoPlayer.Play();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# stARi-zanati
+ Location based AR application that shows some interesting old crafts on baščaršija. 
