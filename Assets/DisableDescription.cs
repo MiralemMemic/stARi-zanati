1ecf750fc5fb91a82cebaef49a549065f7d088fe
MiralemMemic <mmemic2@etf.unsa.ba>
2021-06-27 13:44:37 +0200

    dodane ikone i objasnjenja

---
+++ Assets/DisableDescription.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableDescription : MonoBehaviour
+{
+    public GameObject description;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        description.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/IconDzamdzija.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconDzamdzija : MonoBehaviour
+{
+
+    public GameObject description;
+
+    Ray ray;
+    RaycastHit hit;
+
+    void Awake()
+    {
+        description.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag == "dzamdzija")
+                {
+                    if(description.activeSelf) description.SetActive(false);
+                    else description.SetActive(true);
+                }
+            }
+        }
+    }
+}
